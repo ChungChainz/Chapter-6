54b8b879eb5f40a185688a9acccc764f4508c379
Chris Molis <chrismolis12@gmail.com>
2018-12-13 13:11:30 -0600

    commit

--- CheckZipsMethod/CheckZipsMethod/Program.cs
+++ CheckZipsMethod/CheckZipsMethod/Program.cs
@@ -4,42 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-namespace CheckZips
+
+namespace CheckZipsMethod
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] zipcodes = { 52800, 52801, 52802, 52803, 52804, 52805, 52806, 52807, 52808, 52809 };
-            int zip;
-            bool delivery = true;
-            int i = 0;
-            Write("Enter the zip code you want to deliver to >>> ");
-            zip = Convert.ToInt32(ReadLine());
 
-            while (i < 10)
+            string zip;
+
+            string displayZips;
+
+            Write("Enter the zip code you want to deliver to >>> ");
+            zip = (ReadLine());
+            displayZips(zip);
+        }
+        private static string displayZips = (string zip); 
+         {   bool delivery = true;
+        int[] zipcodes = { 52800, 52801, 52802, 52803, 52804, 52805, 52806, 52807, 52808, 52809 };
+        int i = 0;
+            while (i< 10)
             {
                 if (zip == zipcodes[i])
                 {
                     delivery = false;
                     WriteLine("This is a valid delivery zip code.");
-                }
+    
                 else
                 {
 
                 }
                 ++i;
-            }
+            
 
                 if (delivery == false)
                 {
 
                 }
                 else if (delivery == true)
-                {
+                   {
                     WriteLine("Can't deliver here, sorry");
-                }
-            }
-        }
+
+        }   
     }
+}              
+        
+   
 
