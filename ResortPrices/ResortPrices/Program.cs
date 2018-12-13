9424701b5f35ae3bfddea04ee7c2d9265f344315
ChungChainz <44850506+ChungChainz@users.noreply.github.com>
2018-12-12 20:17:07 -0600

    Update Program.cs

--- ResortPrices/ResortPrices/Program.cs
+++ ResortPrices/ResortPrices/Program.cs
@@ -14,6 +14,7 @@ namespace ResortPrices
             int nights;
             int[] prices = { 200, 200, 180, 180, 160, 160, 160, 145 };
             int total = 0;
+            int i = 0;
            
             Write("How many nights have you stayed at 'The Carefree Resort'?");
             nights = Convert.ToInt32(ReadLine());
@@ -26,7 +27,25 @@ namespace ResortPrices
             {
                 total += prices[8] * nights;
             }
-            
+             WriteLine();
+            WriteLine("These are the nightly rates");
+            WriteLine("________________________________");
+            for (i = 0; i < 9; ++i)
+            {
+                if (i < 8)
+                {
+                    WriteLine("The price for " + i + " night(s) is $" + prices[i]);
+                }
+                else
+                {
+                    WriteLine("The price for 8 nights or more is $" + prices[7]);
+                }
+
+            }
+
+           
+            WriteLine("\n _________________________________");
+            WriteLine("Your total for " + nights + " nights stayed is " + total.ToString("C"));
         }
     }
 }
